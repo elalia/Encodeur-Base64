0c941f95a3c6f166f2cb0938ad4192e3de4a4a41
elalia <37341164+elalia@users.noreply.github.com>
2021-12-20 19:10:28 -0500

    my first commit

---
+++ EncodeBase64.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodeurBase64
+{
+    public class EncodeBase64
+    {
+        private readonly static string[] tab = {"A","B","C","D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+                             "0","1","2","3","4","5","6","7","8","9","+","/"};        
+        
+        public static string Encode(byte[] source)
+        {
+            if (!(source.Length % 3 == 0))
+            {
+                throw new Exception("Le tableau doit avoir une longueur qui sera un multiple de 3 afin de simplifier le test.");
+            }
+
+            StringBuilder stringOfBits = new StringBuilder();
+ 
+            foreach (var bit in source)
+            {
+                var nbrbit = Convert.ToString(bit, 2).Length;
+                while (nbrbit < 8)
+                {
+                    stringOfBits.Append('0');
+                    nbrbit++;
+                }
+                stringOfBits.Append(Convert.ToString(bit, 2));
+            }
+           
+
+            StringBuilder result = new StringBuilder();
+            string VBG;
+            while(stringOfBits.Length>0)
+            {
+                VBG = stringOfBits.ToString().Substring(0, 6);
+                int B = Convert.ToInt32(VBG, 2);
+                result.Append(tab[B]);
+                stringOfBits = stringOfBits.Remove(0, 6);
+            }
+           
+            return result.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodeurBase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] tt = { 0x33, 0xA6, 0xFF};
+            bool compar;
+            var base64 = EncodeBase64.Encode(tt);
+            if (base64 == Convert.ToBase64String(tt))
+                compar = true;
+            else
+                compar = false;
+            var result = compar;           
+        }        
+    }
+}
+    
+
+
